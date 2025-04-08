245544e066bc6995a279d6384f69ccf023de7d34
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-08 20:37:29 -0300

    Update exercicio 28.py

--- exercicio 28.py
+++ exercicio 28.py
@@ -2,21 +2,20 @@ n1 = int(input('Digite o primeiro valor:'))
 n2 = int(input('Digite o segundo valor:'))
 opcoes = (1, 2, 3, 4, 5)
 
-operacao = input('Escolha a operação que deseja realizar: \n'
-                 '[1] Somar \n'
-                 '[2] Multiplicar \n'
-                 '[3] Ver o maior \n'
-                 '[4] Novos números \n'
-                 '[5] Sair do programa')
-while operacao == opcoes:
+    print('\n[1] Somar\n[2] Multiplicar\n[3] Ver o maior\n[4] Novos números\n[5] Sair')
+    operacao = int(input('Escolha a operação: '))
+    
     if operacao == 1:
-        print(f'A soma de {n1} + {n2} = {n1 + n2}.')
+        print(f'Soma: {n1 + n2}')
     elif operacao == 2:
-        print(f'A multiplicação de {n1} x {n2} = {n1 * n2}')
+        print(f'Multiplicação: {n1 * n2}')
     elif operacao == 3:
-        print(f'O maior numero entre {n1} e {n2} é {max(n1, n2)}')
+        print(f'Maior: {max(n1, n2)}')
     elif operacao == 4:
-        n1 = int(input('Digite o primeiro valor:'))
-        n2 = int(input('Digite o segundo valor:'))
+        n1 = int(input('Novo valor 1: '))
+        n2 = int(input('Novo valor 2: '))
     elif operacao == 5:
+        print('Saindo...')
         break
+    else:
+        print('Opção inválida.')
