77dca7d91951754d956e99187250613a8984e13f
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-10 21:15:49 -0300

    exercicio 35.py

--- exercicio 35.py
+++ exercicio 35.py
@@ -1,13 +1,16 @@
-a = input('Digite o 1º valor:')
-b = input('Digite o 2º valor:')
-c = input('Digite o 3º valor:')
-d = input('Digite o 4º valor:')
-e = input('Digite o 5º valor:')
+a = int(input('Digite o 1º valor: '))
+b = int(input('Digite o 2º valor: '))
+c = int(input('Digite o 3º valor: '))
+d = int(input('Digite o 4º valor: '))
+e = int(input('Digite o 5º valor: '))
+
 tupla = (a, b, c, d, e)
-contador9 = tupla.count('9')
-print(f'O valor 9 apareceu {contador9} vezes')
-if '3' not in tupla:
-    print('O valor 3 não foi digitado.')
+
+contador9 = tupla.count(9)
+print(f'O valor 9 apareceu {contador9} vezes.')
+
+if 3 in tupla:
+    posicao3 = tupla.index(3)
+    print(f'O valor 3 apareceu pela primeira vez na {posicao3 + 1}ª posição.')
 else:
-    posição3 = tupla.index('3')
-    print(f'O valor 3 aparece a primeira vez na {posição3 + 1}º posição.')
+    print('O valor 3 não foi digitado.')
