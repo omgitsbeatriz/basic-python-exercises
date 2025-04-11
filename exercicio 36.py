2baeb17e1ab5143e9835d5b527211aefaf201802
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-10 21:15:21 -0300

    exercicio 36.py

--- exercicio 36.py
+++ exercicio 36.py
@@ -1,9 +1,13 @@
 valores = []
-for c in range(0,5):
-    valores.append(int(input('Digite um valor')))
+
+for c in range(5):
+    valores.append(int(input('Digite um valor: ')))
+
 for c, v in enumerate(valores):
     print(f'O valor {v} está na posição {c}')
+
 maior = max(valores)
 menor = min(valores)
-print(f'O maior valor da lista é o {maior} e está na posição {valores.index(maior)}')
-print(f'O menor valor da lista é o {menor} e o menor valor é o {valores.index(menor)}')
+
+print(f'O maior valor da lista é {maior} e está na posição {valores.index(maior)}')
+print(f'O menor valor da lista é {menor} e está na posição {valores.index(menor)}')
