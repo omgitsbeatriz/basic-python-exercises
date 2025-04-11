b5d7b1d89c388b8f4f272244066a7dbb2f0beea3
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-10 21:14:31 -0300

    exercicio 37.py

--- exercicio 37.py
+++ exercicio 37.py
@@ -1,18 +1,25 @@
 lista = []
+
 while True:
-    escolha = input('Para adicionar  um número em sua lista digite A \n'
-                    'Para sair da lista digite X ')
-    escolha = escolha.lower()
+    escolha = input(
+        'Para adicionar um número à lista, digite (A)\n'
+        'Para sair da lista, digite (X): '
+    ).strip().lower()
+
     if escolha == 'a':
-        numero = int(input('Digite o número que deseja adicionar:'))
-        if numero in lista:
-            print(f'Numero {numero} já se encontra na lista.')
-        elif numero not in lista:
-            lista.append(numero)
+        try:
+            numero = int(input('Digite o número que deseja adicionar: '))
+            if numero in lista:
+                print(f'O número {numero} já está na lista.')
+            else:
+                lista.append(numero)
+        except ValueError:
+            print('Digite um número válido.')
     elif escolha == 'x':
         print('Você saiu da lista.')
         break
     else:
-        print('Digite um comando valido!')
+        print('Digite um comando válido!')
+
 lista.sort()
-print(f'Os valores digitados foram : {lista}')
+print(f'\nOs valores digitados foram: {lista}')
