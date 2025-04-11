03e4dee5fe6dd00872b0c39a55e05b5b80287d3e
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-10 21:13:58 -0300

    exercicio 39.py

--- exercicio 39.py
+++ exercicio 39.py
@@ -1,22 +1,27 @@
 lista = []
 contador = 0
 contador5 = 0
+
 while True:
-    escolha = input('Para adicionar um número em sua lista digite (A) \n'
-                      'Para sair e conferir os valores adicionados digite (X)')
-    if escolha in 'aA':
-        numero = int(input('Digite o numero que deseja adicionar:'))
+    escolha = input(
+        'Para adicionar um número na lista, digite (A)\n'
+        'Para sair e conferir os valores, digite (X): '
+    ).strip().upper()
+
+    if escolha == 'A':
+        numero = int(input('Digite o número que deseja adicionar: '))
         lista.append(numero)
         contador += 1
         if numero == 5:
             contador5 += 1
-    elif escolha in 'Xx':
-        if 5 in lista:
-            print(f'A sua lista em ordem decrescente é: {sorted(lista, reverse=True)} \n'
-              f'foram digitados {contador} números, e o 5 foi digitado {contador5} vezes.')     
-        elif 5 not in lista:
-            print(f'A sua lista em ordem decrescente é: {sorted(lista, reverse=True)} \n'
-              f'foram digitados {contador} números, e o 5 não foi digitado.') 
+    elif escolha == 'X':
+        lista_ordenada = sorted(lista, reverse=True)
+        print(f'\nA sua lista em ordem decrescente é: {lista_ordenada}')
+        print(f'Foram digitados {contador} números.')
+        if contador5 > 0:
+            print(f'O número 5 foi digitado {contador5} vezes.')
+        else:
+            print('O número 5 não foi digitado.')
         break
     else:
-        print('Digite um comando válido')
+        print('Comando inválido. Tente novamente.')
